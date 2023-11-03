0fdf12f6875d83c0e0c5b7f0a60568acf6153075
Yichen Zhang <yzhang183@slb.com>
2023-11-03 14:19:33 +0800

    refactor: make isValidBarcode and generateTag simplier

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -1,17 +1,17 @@
-import {loadAllItems, loadPromotions} from './Dependencies'
+import { loadAllItems, loadPromotions } from './Dependencies'
 
 const allItems = loadAllItems()
 const allDiscounts = loadPromotions()
 
-export interface Tag{
-  barcode:string;
+export interface Tag {
+  barcode: string;
   quantity: number;
 }
 
-export interface ItemProperty{
-  name:string;
+export interface ItemProperty {
+  name: string;
   quantity: number;
-  unit:string;
+  unit: string;
   unitPrice: number;
   subtotal: number;
   discounted: number;
@@ -28,7 +28,7 @@ export function printReceipt(tags: string[]): string {
 
 function generateItemQuantityMap(items: string[]): Map<string, number> {
   const processedItems = new Map<string, number>()
-  for(const item of items){
+  for (const item of items) {
     const tag = generateTag(item)
     const currentQuantity = processedItems.get(tag.barcode) ?? 0
     processedItems.set(tag.barcode, currentQuantity + tag.quantity)
@@ -36,10 +36,9 @@ function generateItemQuantityMap(items: string[]): Map<string, number> {
   return processedItems
 }
 
-function generateTag(item: string) : Tag {
-  const seperatorIndex = item.indexOf('-')
-  const barcode = seperatorIndex > 0 ? item.substring(0, seperatorIndex) : item
-  const quantityStr = seperatorIndex > 0 ? item.substring(seperatorIndex+1) : '1'
+function generateTag(item: string): Tag {
+  const [barcode, quantityStr = '1'] = item.split('-')
+
   if (!isValidBarcode(barcode)) {
     console.log(`Invalid Barcode: ${barcode}.`)
     throw new Error(`Invalid Barcode: ${barcode}.`)
@@ -56,45 +55,40 @@ function generateTag(item: string) : Tag {
   return tag
 }
 
-function isValidQuantity(quantity: string) : boolean{
+function isValidQuantity(quantity: string): boolean {
   const quantityNum = parseInt(quantity)
-  if(!isNaN(quantityNum)){
-    if(quantityNum > 0){
+  if (!isNaN(quantityNum)) {
+    if (quantityNum > 0) {
       return true
     }
   }
   return false
 }
 
-function isValidBarcode(barcode: string) : boolean{
-  for(const item of allItems){
-    if(barcode === item.barcode){
-      return true
-    }
-  }
-  return false
+function isValidBarcode(barcode: string): boolean {
+  return !!allItems.find((item) => item.barcode === barcode)
 }
 
-function generateItemProperties(itemQuantityMap:Map<string, number>){
-  const itemProperties : ItemProperty[] = []
+function generateItemProperties(itemQuantityMap: Map<string, number>) {
+  const itemProperties: ItemProperty[] = []
   for (const [barcode, quantity] of itemQuantityMap) {
     itemProperties.push(generateSingleItemProperty(barcode, quantity))
   }
   return itemProperties
 }
 
-function getItemDetail(barcode: string){
-  for(const item of allItems){
-    if(barcode === item.barcode){
+function getItemDetail(barcode: string) {
+  for (const item of allItems) {
+    if (barcode === item.barcode) {
       return item
     }
   }
   return null
 }
 
-function generateSingleItemProperty(barcode: string, quantity: number):ItemProperty{
+function generateSingleItemProperty(barcode: string, quantity: number): ItemProperty {
   const itemDetail = getItemDetail(barcode)
-  if(itemDetail === null){
+  if (itemDetail === null) {
     throw new Error()//not possible
   }
   const itemProperty = {
@@ -108,12 +102,12 @@ function generateSingleItemProperty(barcode: string, quantity: number):ItemPrope
   return itemProperty
 }
 
-function getDiscount(barcode:string, quantity: number, unitPrice: number){
-  for(const discount of allDiscounts){
-    for(const discountBarcode of discount.barcodes){
-      if(discountBarcode === barcode){
-        if(discount.type === 'BUY_TWO_GET_ONE_FREE'){
-          return Math.floor(quantity/3) * unitPrice
+function getDiscount(barcode: string, quantity: number, unitPrice: number) {
+  for (const discount of allDiscounts) {
+    for (const discountBarcode of discount.barcodes) {
+      if (discountBarcode === barcode) {
+        if (discount.type === 'BUY_TWO_GET_ONE_FREE') {
+          return Math.floor(quantity / 3) * unitPrice
         }
       }
     }
@@ -121,7 +115,7 @@ function getDiscount(barcode:string, quantity: number, unitPrice: number){
   return 0
 }
 
-function render(itemProperties:ItemProperty[]) : string{
+function render(itemProperties: ItemProperty[]): string {
   let receipt = '***<store earning no money>Receipt ***\n'
   const totalDiscount = getTotalDiscount(itemProperties)
   const totalPrice = getTotalPrice(itemProperties) - totalDiscount
@@ -136,17 +130,17 @@ function getReceiptLine(itemProperty: ItemProperty): string {
   return `Name: ${itemProperty.name}, Quantity: ${itemProperty.quantity} ${itemProperty.unit}s, Unit: ${itemProperty.unitPrice.toFixed(2)}(yuan), Subtotal: ${(itemProperty.subtotal - itemProperty.discounted).toFixed(2)}(yuan)`
 }
 
-function getTotalPrice(itemProperties:ItemProperty[]){
+function getTotalPrice(itemProperties: ItemProperty[]) {
   let totalPrice = 0
-  for(const itemProperty of itemProperties){
+  for (const itemProperty of itemProperties) {
     totalPrice += itemProperty.subtotal
   }
   return totalPrice
 }
 
-function getTotalDiscount(itemProperties:ItemProperty[]){
+function getTotalDiscount(itemProperties: ItemProperty[]) {
   let totalDiscount = 0
-  for(const itemProperty of itemProperties){
+  for (const itemProperty of itemProperties) {
     totalDiscount += itemProperty.discounted
   }
   return totalDiscount
